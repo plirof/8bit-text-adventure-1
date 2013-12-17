e7642d51b047cf59ef17f1ea1a484f51cc6ec065
Matthew Taylor <mstaveleytaylor@gmail.com>
2013-12-17 12:59:05 +0000

    scriptNew.js added

---
+++ web-projects-remote/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() { //IGNORE THIS
+	$('#button').click(function() { //AND THIS
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	});//IGNORE THIS TOO
+}); // DO NOT TYPE ANYTHING AFTER THIS
