ec852ee1d49dca25b78bf4c2d0b3d635dc1ed6a0
bobbiy <bobbie.rausch@me.com>
2013-12-21 23:05:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,20 +13,34 @@ var fightMode = false; //This makes you fight. While you fight, no time goes by.
 var pots = 0; //Pots is short for potions. If you have potions, you can use them to regain life.
 //END OF VARIABLE AREA
 
-
 //DEFINING AREA!!!! THESE ARE FUNCTIONS AND ARE NOT ACTIVE UNTIL CALLED
 function getInput() {
-	//$(document).ready(function() {
 		$('#command').focus();		
 		var userRaw = $('#command').val();
 		return userRaw;
-	//});
-	
 }     
+
+function addText(text) {;
+    $("#main").scrollTop($("#main")[0].scrollHeight); //scrolls down
+ }
 function printStart() {
 	$(document).ready(function() {
 		$("#main").append(">You wake up on a small island. This island is so small that you can see every bank from your current vantage point. There is a broken boat, a generator (that your not sure if works), banana trees, sharp-edged agave plants and a cave that looks unexplored.<br>");
 	});
+}
+
+function findCurrentPlace() {
+	switch (currentPlace) {
+		case "home":
+			moveToHome();
+		break;
+		case "cave":
+		
+		break;
+		case "generator":
+		
+		break;
+	}
 }
   
 function fightCheckInput(input) {
@@ -239,9 +253,7 @@ function checkDays() {
                 }
 }
 //Move to home function
-function moveToHome(){
-	var newUserRaw = "";
-					
+function moveToHome() {
 				do {	
 						
 						var newUserRaw = getInput();
@@ -250,91 +262,94 @@ function moveToHome(){
 
                         user = newUserRaw.toLowerCase();
                         inputInvalid = false;
-                        $('#main').append(user+"<br>");
+                        $('#main').append("&gt;" + user + "<br>");
                 } while (inputInvalid);
-                
-                //If the user pressed cancel on the prompt box, break out of the current while loop.
-                if (cancel){
-                        break;
-            }
             //Checks to see if the first five letters entered were drop and a space - If so, run remItem()function with the user's 5 letter onwards (after "drop ")
-            if (user.slice(0,5) === "drop ") {
-                remItem(user.slice(5));
-            } else {
-                                //Else, does all the other checks to see what the user has typed.
-                switch(user){
-                        case 'help':
-                                $('#main').append("loot [CONTAINER]<br>Loots the specified container.<br><br>");
-                                $('#main').append("examine [ITEM/WEAPON/PLACE]<br>Examines the specified item or place name.<br><br>"); 
-                                $('#main').append("inv [ITEM/WEAPON]<br>Short for inventory, adds the selected item or weapon to your inventory.<br><br>");
-                                $('#main').append("drop [ITEM/WEAPON]<br>Drops the selected item or weapon. It must be in your inventory before you can drop it. If you drop a weapon then it will be removed from your invetory and you cannot kill with it.<br><br>");
-                                $('#main').append("jump<br>Makes your character jump.<br>");
-                                $('#main').append("look around<br>Your character surveys the area. Using this, you can find things you wouldn't normally see.<br><br>");
-                                $('#main').append("move to [PLACE]<br>Moves your character to the specified PLACE. Only some areas are acessible from other areas.<br><br>");
-                                $('#main').append("Types of objects:<br><br>");
-                                $('#main').append("ITEM<br>E.G agave plants are ITEMs. You can put them in your iventory, examine them, or drop them.<br><br>");
-                                $('#main').append("WEAPON<br>Agave plants are also weapons...?<br><br>");
-                                $('#main').append("PLACE<br>You can go inside these.<br><br>");
-                                $('#main').append("CONTAINER<br>You can loot these and gain ITEMS.<br><br>");
-                                $('#main').append("ENTITY<br>These are humans or animals. You can kill other ENTITYs.<br>");
-                        break;
-                        case 'look around':
-                                $("#main").append(">The agaves and the banana trees are everywhere, in the north (n) is the generator, the boat is in the southeast (se), and the cave is in the west (w)<br>");
-                                timeCheck();
-                        break;
-                        case 'jump':
-                                $("#main").append(">You jump up for some reason you don't really know. You get some pretty nice air, and you see that there is an island right next to the one your on in the south.<br>");
-                                timeCheck();
-                        break;
-                        case 'inv agave leaf':
-                                if (invCheck()) {
-
-                                        $('#main').append(">Your pockets are full. You have to >drop [ITEM_NAME] before picking this item up<br>");
-                                }
-                                else {
-                                        $('#main').append(">You picked up a jagged agave leaf. This is a  weapon; However, it is only a 1/20 attack, not very good compared to a steel-tempered ulfberht.<br>");
-
-                                        //Set hitpoints to 1, and set current weapon to agave leaf
-                                        atk = [1, "agave leaf"];
-                                        //Adds one to the time and checks it
-                                        timeCheck();
-                                        addInv("agave leaf");
-										//Uncomment next line for debugging the add function
-                                        //$('#main').append(inv); 
- 
-                                }   
-                        break;
-                        case "quit":
-                                //Calls printGameOver() and then and exits the function. (using return makes the rest of the function unreachable)
-                                printGameOver();
-                                return;
-                        //No break is needed here because return exits the function
-                        //Checks all the places that can be moved to next.
-                        case "move to generator":
-                                $('#main').append("You walk over to the generator.<br>");
-                                currentPlace = "generator";
-                                moveToGenerator(); //Not implemented yet
-                        break;
-                        
-                        case "move to cave":
-                                $('#main').append("You walk over to the cave.<br>");
-
-                                currentPlace = "cave";
-                                moveToCave(); //Not implemented yet
-                        break;
-                        default :
-                                //If the user typed none of the above, logs "Misunderstood command."
-                                $('#main').append(">Misunderstood command.<br>");
-
-            }
-		}
-    }
+    if (user.slice(0,5) === "drop ") {
+        remItem(user.slice(5));
+    } else {
+        //Else, does all the other checks to see what the user has typed.
+        switch(user){
+            case 'help':
+                $('#main').append("loot [CONTAINER]<br>Loots the specified container.<br><br>");
+                $('#main').append("examine [ITEM/WEAPON/PLACE]<br>Examines the specified item or place name.<br><br>"); 
+                $('#main').append("inv [ITEM/WEAPON]<br>Short for inventory, adds the selected item or weapon to your inventory.<br><br>");
+                $('#main').append("drop [ITEM/WEAPON]<br>Drops the selected item or weapon. It must be in your inventory before you can drop it. If you drop a weapon then it will be removed from your invetory and you cannot kill with it.<br><br>");
+                $('#main').append("jump<br>Makes your character jump.<br>");
+                $('#main').append("look around<br>Your character surveys the area. Using this, you can find things you wouldn't normally see.<br><br>");
+                $('#main').append("move to [PLACE]<br>Moves your character to the specified PLACE. Only some areas are acessible from other areas.<br><br>");
+                $('#main').append("Types of objects:<br><br>");
+                $('#main').append("ITEM<br>E.G agave plants are ITEMs. You can put them in your iventory, examine them, or drop them.<br><br>");
+                $('#main').append("WEAPON<br>Agave plants are also weapons...?<br><br>");
+                $('#main').append("PLACE<br>You can go inside these.<br><br>");
+                $('#main').append("CONTAINER<br>You can loot these and gain ITEMS.<br><br>");
+                $('#main').append("ENTITY<br>These are humans or animals. You can kill other ENTITYs.<br>");
+                addText();
+            break;
+            case 'look around':
+                $("#main").append(">The agaves and the banana trees are everywhere, in the north (n) is the generator, the boat is in the southeast (se), and the cave is in the west (w)<br>");
+                timeCheck();
+                addText();
+            break;
+            case 'jump':
+                $("#main").append(">You jump up for some reason you don't really know. You get some pretty nice air, and you see that there is an island right next to the one your on in the south.<br>");
+                timeCheck();
+                addText();
+            break;
+            case 'inv agave leaf':
+                if (invCheck()) {
+                    $('#main').append(">Your pockets are full. You have to >drop [ITEM_NAME] before picking this item up<br>");
+                    addText();
+                }
+                else {
+                    $('#main').append(">You picked up a jagged agave leaf. This is a  weapon; However, it is only a 1/20 attack, not very good compared to a steel-tempered ulfberht.<br>");
+                    addText();
+                    //Set hitpoints to 1, and set current weapon to agave leaf
+                    atk = [1, "agave leaf"];
+                    //Adds one to the time and checks it
+                    timeCheck();
+                    addInv("agave leaf");
+					//Uncomment next line for debugging the add function
+                    //$('#main').append(inv); 
+                }   
+            break;
+            case "quit":
+                //Calls printGameOver() and then and exits the function. (using return makes the rest of the function unreachable)
+                printGameOver();
+                return;
+                //No break is needed here because return exits the function
+            //Checks all the places that can be moved to next.
+            case "move to generator":
+                $('#main').append("You walk over to the generator.<br>");
+                addText();
+                currentPlace = "generator";
+                moveToGenerator(); //Not implemented yet
+            break;       
+            case "move to cave":
+            $('#main').append("You walk over to the cave.<br>");
+            addText();
+            currentPlace = "cave";
+            moveToCave(); //Not implemented yet
+            break;
+            default :
+            //If the user typed none of the above, logs "Misunderstood command."
+            $('#main').append(">Misunderstood command.<br>");
+            addText();
+        }
+	}
+}
  
  
  
 //END OF DEFINING AREA
 
 printStart();
+
+$(document).keydown(function(key) {
+	if (parseInt(key.which,10) === 13) {
+		findCurrentPlace();
+	}
+});
 
 //Emails:
 //deluz@esedona.net - Gabriel de Luz (JS Dev)
