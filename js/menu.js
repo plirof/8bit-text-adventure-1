3f6333f11b38e877390f33fe205063ce9e232472
techhead55 <techhead55@gmail.com>
2014-04-24 19:43:47 +1000

    Introduced footer styling and created style classes for menu content. Basic styling for the game io was done.

--- js/menu.js
+++ js/menu.js
@@ -6,13 +6,28 @@ window.onload = function(){
         $("#header").animate({
             width: ""+(window.innerWidth-300)
         }, 300 );
+        $("#footer").animate({
+            width: ""+(window.innerWidth-300)
+        }, 300 );
     }, function() {
         $(this).animate({
             marginLeft: "-50px"
         }, 300 );
         $("#header").animate({
-            width: ""+(window.innerWidth)
+            width: ""+(window.innerWidth-50)
+        }, 300 );
+        $("#footer").animate({
+            width: ""+(window.innerWidth-50)
         }, 300 );
     });
+    document.getElementById("header").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("footer").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("game").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("game").style.height = (window.innerHeight-100)+"px";
 };
-
+window.onresize = function(){
+    document.getElementById("header").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("footer").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("game").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("game").style.height = (window.innerHeight-100)+"px";
+};
