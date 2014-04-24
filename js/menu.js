fd4e632a847a96477606816bf68000a0a0b9f21f
techhead55 <techhead55@gmail.com>
2014-04-24 21:10:38 +1000

    Completed styling for image display.

--- js/menu.js
+++ js/menu.js
@@ -18,6 +18,12 @@ window.onload = function(){
         $("#game-input").animate({
             width: ""+(window.innerWidth-340)
         }, 300 );
+        $("#logo").animate({
+            right: "50px"
+        }, 300 );
+        $("#game-output-display").animate({
+            width: ""+(window.innerWidth-300)
+        }, 300 );
     }, function() {
         $(this).animate({
             marginLeft: "-50px"
@@ -37,15 +43,23 @@ window.onload = function(){
         $("#game-input").animate({
             width: ""+(window.innerWidth-90)
         }, 300 );
+        $("#logo").animate({
+            right: "-250px"
+        }, 300 );
+        $("#game-output-display").animate({
+            width: ""+(window.innerWidth-50)
+        }, 300 );
     });
     document.getElementById("header").style.width = (window.innerWidth-50)+"px";
     document.getElementById("footer").style.width = (window.innerWidth-50)+"px";
     document.getElementById("game").style.width = (window.innerWidth-50)+"px";
+    document.getElementById("game-output-display").style.width = (window.innerWidth-50)+"px";
     document.getElementById("game").style.height = (window.innerHeight-70)+"px";
     document.getElementById("game-output").style.height = (window.innerHeight-260)+"px";
     document.getElementById("game-input").style.width = (window.innerWidth-90)+"px";
 };
 window.onresize = function(){
+    document.getElementById("game-output-display").style.width = (window.innerWidth-50)+"px";
     document.getElementById("header").style.width = (window.innerWidth-50)+"px";
     document.getElementById("footer").style.width = (window.innerWidth-50)+"px";
     document.getElementById("game").style.width = (window.innerWidth-50)+"px";
